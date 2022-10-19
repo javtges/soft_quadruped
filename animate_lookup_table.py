80b6a8ecaadfd9ac4c9f52c6a36cacf1b7357d96
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-10-19 09:52:37 -0500

    made animation gif, apriltag testfile

--- animate_lookup_table.py
+++ animate_lookup_table.py
@@ -1,4 +1,57 @@
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import pandas as pd
+
+
+filename_default = "table_221018_154600_"
+num = 0
+num_max = 20
+
+x_list = []
+z_list = []
+y_list = []
+
+for i in range(num_max):
+    filename = filename_default + str(i)
+    data = pd.read_csv(filename).values
+    data = data[1:][:]
+    print(data.shape)
+    x = data[:, [4],].T
+    print(x.shape)
+    # print(x)
+    y = data[:, [5],].T
+    z = data[:, [6],].T
+    
+    
+    x = x[0].tolist()
+    y = y[0].tolist()
+    z = z[0].tolist()
+    x_list.append(x)
+    y_list.append(y)
+    z_list.append(z)
+
+
+
+fig, ax = plt.subplots()
+def animate(i):
+    fig.clear()
+    ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-0.005, 0.005), ylim=(-0.005, 0.005))
+    ax.set_xlim(-0.005,0.005)
+    ax.set_ylim(-0.005,0.005)
+    ax.grid(b=None)
+    ax.set_xlabel('x [m]')
+    ax.set_ylabel('y [m]')
+    ax.set_title("HSA In-Plane Displacement")
+    ax.text(0.02, 0.95, 'Time Step = %d' % i, transform=ax.transAxes)
+    s = ax.scatter(y_list[i][:], z_list[i][:], cmap = "RdBu_r", marker = ".", edgecolor = None)
+    
+
+ani = animation.FuncAnimation(fig, animate, interval=1000, frames=range(num_max))
+
+FFwriter = animation.FFMpegWriter(fps=3)
+ani.save('animation.mp4', writer=FFwriter)
+
+
+    
---
+++ apriltag_test.py
@@ -0,0 +1,82 @@
+# import serial
+import cv2
+import pyrealsense2 as rs
+import numpy as np
+import yaml
+import time
+import readchar
+import threading
+from pupil_apriltags import Detector
+
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+pipeline = rs.pipeline()
+config = rs.config()
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+#########################################
+
+# Start streaming
+cfg = pipeline.start(config)
+profile = cfg.get_stream(rs.stream.color)
+intr = profile.as_video_stream_profile().get_intrinsics()
+print(intr)
+pause_flag = 0
+tag_xyz = np.zeros((3,1))
+
+while True:
+
+        
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    
+    if not depth_frame or not color_frame:
+        continue
+
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+        
+    gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
+    tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=[intr.fx, intr.fy, intr.ppx, intr.ppy], tag_size=0.055)
+    # print(tags)
+
+    if len(tags) != 0:
+        tag_xyz = tags[0].pose_t
+        tag_R = tags[0].pose_R
+        tag_xyz = np.array(tag_xyz)
+        tag_R = np.array(tag_R)
+        print(tag_xyz)
+        print(tag_R)
+
+        
+
+    
