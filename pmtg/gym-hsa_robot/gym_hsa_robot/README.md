e5ac955621f21b40e36f5afa49ad467b55089b3b
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-11-04 11:40:53 -0500

    aaaa

--- pmtg/gym-hsa_robot/gym_hsa_robot/README.md
+++ pmtg/gym-hsa_robot/gym_hsa_robot/README.md
@@ -1,3 +1,12 @@
 ## HSA Robot Environment Resources
 
 This is where urdfs will go, and other helper python files for the environment.
+
+
+
+ARS RL Algorithm Pseudocode:
+
+Function to test environment - for N simulation steps
+Test positive and negative rewards for random perturbation of weight vector
+Sort directions that have the highest weights
+Need some sort of normalization of states
--- pmtg/gym-hsa_robot/gym_hsa_robot/envs/hsa_robot_env2.py
+++ pmtg/gym-hsa_robot/gym_hsa_robot/envs/hsa_robot_env2.py
@@ -4,17 +4,18 @@ import pybullet as p
 import math
 from gym_hsa_robot.resources.plane import Plane
 from gym_hsa_robot.resources.hsa_robot import HSARobot
+import matplotlib.pyplot as plt
 
 
 class HSARobot_Env(gym.Env):
     
-    metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human', 'rgb_array'], 'video.frames_per_second': 20 }
 
     
     def __init__(self):
         
-        # self.client = p.connect(p.GUI)
-        self.client = p.connect(p.DIRECT)
+        self.client = p.connect(p.GUI)
+        # self.client = p.connect(p.DIRECT)
         p.setTimeStep(1/240, self.client)
         
         # Here, define my action space and my observation space
@@ -65,8 +66,33 @@ class HSARobot_Env(gym.Env):
 
         return np.array(robot_ob, dtype=np.float32)
     
-    def render(self):
-        pass
+    def render(self, mode='human'):
+        if self.rendered_img is None:
+            self.rendered_img = plt.imshow(np.zeros((100, 100, 4)))
+
+        # Base information
+        robot_id, client_id = self.robot.get_ids()
+        proj_matrix = p.computeProjectionMatrixFOV(fov=80, aspect=1,
+                                                   nearVal=0.01, farVal=100)
+        pos, ori = [list(l) for l in
+                    p.getBasePositionAndOrientation(robot_id, client_id)]
+        pos[2] = 0.2
+
+        # Rotate camera direction
+        rot_mat = np.array(p.getMatrixFromQuaternion(ori)).reshape(3, 3)
+        camera_vec = np.matmul(rot_mat, [1, 0, 0])
+        up_vec = np.matmul(rot_mat, np.array([0, 0, 1]))
+        # view_matrix = p.computeViewMatrix(pos, pos + camera_vec, up_vec)
+
+        view_matrix = p.computeViewMatrix([0,1,0.5], [0,0,0], up_vec)
+
+        # Display image
+        frame = p.getCameraImage(100, 100, view_matrix, proj_matrix)[2]
+        # frame = p.getCameraImage(100, 100)[2]
+        frame = np.reshape(frame, (100, 100, 4))
+        self.rendered_img.set_data(frame)
+        plt.draw()
+        plt.pause(.00001)
     
     def close(self):
         p.disconnect((self.client))
