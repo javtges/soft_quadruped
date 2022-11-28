375156d89127b0861cd772f764e27740bb261206
James Avtges <jamesavtges2022@u.northwestern.edu>
2022-11-28 12:54:48 -0600

    made new animation tools

--- animate_lookup_table.py
+++ animate_lookup_table.py
@@ -1,6 +1,7 @@
 import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import csv
 import pandas as pd
 
@@ -19,24 +20,31 @@ y_list = []
 
 for i in range(num_max):
     filename = filename_default + str(i)
-    data = pd.read_csv(filename).values
-    data = data[1:][:]
-    print(data.shape)
-    x = data[:, [4],].T
+    print(filename)
+    # data = pd.read_csv(filename).values
+    data = np.genfromtxt(filename, delimiter=',')
+    x = data[:,4] # this should be X
+    y = data[:,5] # this should be Y
+    z = data[:,6] # this should be Z in the camera frame (Y in the planar frame)
+    # data = data[1:][:]
+    # print(data.shape)
+    
     print(x.shape)
     # print(x)
-    y = data[:, [5],].T
-    z = data[:, [6],].T
     
-    
-    x = x[0].tolist()
-    y = y[0].tolist()
-    z = z[0].tolist()
-    x_list.append(x)
-    y_list.append(y)
-    z_list.append(z)
+    x = x.tolist()
+    y = y.tolist()
+    z = z.tolist()
 
+    x_list += x
+    y_list += y
+    z_list += z
+    # x_list.append(x)
+    # y_list.append(y)
+    # z_list.append(z)
 
+# print(x_list)
+print(len(x_list))
 
 fig, ax = plt.subplots()
 def animate(i):
@@ -52,10 +60,56 @@ def animate(i):
     s = ax.scatter(x_list[i][:], z_list[i][:], cmap = "RdBu_r", marker = ".", edgecolor = None)
     
 
-ani = animation.FuncAnimation(fig, animate, interval=1000, frames=range(num_max))
+# ani = animation.FuncAnimation(fig, animate, interval=1000, frames=range(num_max))
+# ani.save('22_11_21_with_reset_xz_newservo.gif', writer='pillow')
 
-# FFwriter = animation.FFMpegWriter(fps=3)
-ani.save('22_11_21_with_reset_xz_newservo.gif', writer='pillow')
+###################################################################################
+
+fig, ax = plt.subplots()
+ax.set_xlabel('X Axis', size = 12)
+ax.set_ylabel('Y Axis', size = 12)
+ax.axis([-0.02,0.02,-0.01,0.01])
+x_vals = []
+y_vals = []
+intensity = []
+iterations = len(x_list)
+n = 0
+
+t_vals = np.linspace(0,1, iterations)
+
+colors = [[0,0,1,0],[0,0,1,0.5],[0,0.2,0.4,1]]
+cmap = LinearSegmentedColormap.from_list("", colors)
+scatter = ax.scatter(x_vals,y_vals, c=[], cmap=cmap, vmin=0,vmax=1)
+
+def get_new_vals():
+    global n
+    x = x_list[n]
+    y = z_list[n]
+    n += 1
+    # print(x, y)
+    return x, y
+
+def update(t):
+    global x_vals, y_vals, intensity
+    # Get intermediate points
+    new_xvals, new_yvals = get_new_vals()
+    # print(new_xvals, new_yvals)
+    x_vals = np.append(x_vals,[new_xvals])
+    y_vals = np.append(y_vals,[new_yvals])
+
+    # Put new values in your plot
+    scatter.set_offsets(np.c_[x_vals,y_vals])
+
+    #calculate new color values
+    intensity = np.concatenate((np.array(intensity)*0.96, np.ones(len([new_xvals]))))
+    scatter.set_array(intensity)
+
+    # Set title
+    ax.set_title('Time: %0.3f' %t)
+
+ani = animation.FuncAnimation(fig, update, frames=t_vals,interval=50)
+filename_out = filename + "points.gif"
+ani.save(filename_out, writer='pillow')
 
 
-    
+plt.show()
