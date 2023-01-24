99c317c65fd23aa9924ccfa18af06aca0b299341
James Avtges <jamesavtges2022@u.northwestern.edu>
2023-01-23 23:59:50 -0500

    more setup.py fixes

--- pmtg/gym-hsa_robot/setup.py
+++ pmtg/gym-hsa_robot/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='gym_hsa_robot',
       version='0.0.1',
       author='James Avtges',
-      packages=find_packages('gym_hsa_robot'),
+      packages=find_packages(),
       install_requires=['gym',
                         'pybullet',
                         'numpy']
